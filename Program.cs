8f3f2515be3fc5843b801251a72971cb9182c013
ILLIBOY <illionairedan@ssem.re.kr>
2016-02-12 17:58:58 +0900

    매개변수를 전달하는 또다른 방법
    :(매개변수명) 이렇게 하면 다른 방법으로 전달할 수 있다.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace method_and_class
+{
+	class MainClass
+	{
+
+		static void profile(string name, string phone)
+		{
+			Console.WriteLine ("name:{0}, phone:{1}", name, phone);
+		}
+
+		public static void Main (string[] args)
+		{
+			profile (name: "chulwon_jung", phone: "010-4063-5296");
+		}
+	}
+}
